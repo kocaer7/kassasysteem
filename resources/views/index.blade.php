26c4874a865000c7e73a319a9d33bc5a8dd1003d
kocaer7 <kocaer7@outlook.com>
2018-11-14 15:11:25 +0100

    Everything works fine.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -51,6 +51,7 @@
                         </tr>
                         @endforeach
                     </table>
+                   {{ $products->links() }}
                 </div>
            </table>
         </tbody>
